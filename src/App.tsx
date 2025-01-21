b78410bf5a49b4cd7f960588d6a4a0d9a1d57a83
krishna-099 <kumarrkrishna017@gmail.com>
2025-01-21 12:39:09 +0530

    commit 1

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/techyons_assignment">
       <Routes>
         <Route path="/" element={<> krishna</>} />
       </Routes>
